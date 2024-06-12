87d74fd72f4dab826fd822200add3682acfef5fb
masud4657 <masudpfrnt@gmail.com>
2024-06-12 21:05:23 +0600

    added the contact us section in home page.

--- src/Components/Home/Faq.jsx
+++ src/Components/Home/Faq.jsx
@@ -5,7 +5,7 @@ const Faq = () => {
         <div>
            <section className="dark:bg-gray-100 dark:text-gray-800">
 	<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
-		<h2 className="text-2xl font-semibold sm:text-4xl">Frequently Asked Questions</h2>
+		<h2 className="text-2xl text-center font-semibold sm:text-4xl">Frequently Asked Questions</h2>
 		<p className="mt-4 mb-8 dark:text-gray-600"></p>
 		<div className="space-y-4">
 			<details className="w-full border rounded-lg">
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Collaborators from "../../Components/Collaborators/Collaborators";
 import AsTeacher from "../../Components/Home/AsTeacher";
 import Banner from "../../Components/Home/Banner";
+import Contact from "../../Components/Home/Contact";
 import Faq from "../../Components/Home/Faq";
 
 const Home = () => {
@@ -23,6 +24,7 @@ classes,Total student enrollment in your website */}
 {/* 5.Added at least 2(two) extra sections on the home page relevant to the
 website. */}
 <Faq></Faq>
+<Contact></Contact>
     </div>
   );
 };
