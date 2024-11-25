29e32d7e1ef55ca200e3df0cf4c8cd8185c472c4
masud467 <masudpfrnt@gmail.com>
2024-11-26 02:54:29 +0600

    update:change title text and font.

--- src/Components/Home/PopularCourses.jsx
+++ src/Components/Home/PopularCourses.jsx
@@ -1,10 +1,11 @@
-
 const PopularCourses = () => {
-    return (
-        <div>
-           <h1>Show popular courses</h1> 
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-medium mt-10">
+        Show popular courses
+      </h1>
+    </div>
+  );
 };
 
 export default PopularCourses;
