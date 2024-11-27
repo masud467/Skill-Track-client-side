75a20f4622f74c3bb32b9976acd11a51a0a32f30
masud467 <masudpfrnt@gmail.com>
2024-11-27 08:00:47 +0600

    remove width and auto margin from main.

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Toaster } from 'react-hot-toast';
-import { HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { HelmetProvider } from "react-helmet-async";
 
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+import AuthProvider from "./Providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import {
-  
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './Routes/Routes';
-import AuthProvider from './Providers/AuthProvider';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
-
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
     <AuthProvider>
-    <QueryClientProvider client={queryClient}>
-    <HelmetProvider>
-    <div className='max-w-7xl mx-auto'>
-    <RouterProvider router={router}></RouterProvider>
-    </div>
-    </HelmetProvider>
-    <Toaster></Toaster>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div>
+            <RouterProvider router={router}></RouterProvider>
+          </div>
+        </HelmetProvider>
+        <Toaster></Toaster>
+      </QueryClientProvider>
     </AuthProvider>
-    
-   
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
