e8723ff586e933a4e8dc4b57a60c0f630e16eeb6
masud467 <masudpfrnt@gmail.com>
2024-11-27 08:03:30 +0600

    update(footer):added social link.

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -2,30 +2,70 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="px-6 mt-10 divide-y bg-base-200 dark:bg-gray-100 dark:text-gray-800">
-        <div className="container flex flex-col justify-evenly py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
-          <div className="lg:w-1/3">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="flex justify-center space-x-3 lg:justify-start"
-            >
-              <div className="flex items-center justify-center w-16 h-16 rounded-full dark:bg-violet-600">
-                <img className="rounded-md" src="/logo1.png" alt="" />
-              </div>
-              <span className="self-center text-2xl font-semibold">
-                SkillTrack
-              </span>
-            </a>
+    <div className="lg:text-start text-center">
+      <footer className="lg:p-16 p-10 mt-10 divide-y-2 bg-base-200 dark:bg-gray-100 dark:text-gray-800">
+        <div className="container flex flex-col justify-between py-10 space-y-10 lg:flex-row lg:space-y-0">
+          <div className="space-y-10">
+            <div>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                className="flex justify-center gap-3 lg:justify-start"
+              >
+                <div className="flex items-center justify-center w-16 h-16 rounded-full dark:bg-violet-600">
+                  <img className="rounded-md" src="/logo1.png" alt="" />
+                </div>
+                <span className="self-center text-2xl font-semibold">
+                  SkillTrack
+                </span>
+              </a>
+            </div>
+            <div className="space-y-1 font-semibold">
+              <p>Road-21, Sector-11, Uttara, Dhaka-1230, Bangladesh</p>
+              <h1>Support: skill-track@gmail.com</h1>
+              <p>(Available : 10:00am to 05:00pm)</p>
+            </div>
           </div>
-          <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
+
+          <div className=" lg:flex lg:gap-6 ">
             <div className="space-y-3">
-              <div className="uppercase dark:text-gray-900">Social media</div>
-              <div className="flex justify-start space-x-3">
+              <div>
+                <Link
+                  to="/"
+                  onClick={() =>
+                    window.scrollTo({ top: 0, behavior: "smooth" })
+                  }
+                >
+                  <p className="font-bold">Home</p>
+                </Link>
+              </div>
+              <div>
+                <a href="#" className="space-y-6">
+                  <p className="font-bold">About Us</p>
+                </a>
+              </div>
+              <div>
+                <a href="#" className="space-y-6">
+                  <p className="font-bold">Privacy Policy</p>
+                </a>
+              </div>
+              <div>
+                <a href="#">
+                  <p className="font-bold">Terms of service</p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="text-sm text-center">
+            <div className="space-y-3">
+              <div className="uppercase dark:text-gray-900 text-center font-bold">
+                Follow Us
+              </div>
+              <div className="flex justify-center space-x-3">
                 <a
                   rel="noopener noreferrer"
-                  href="#"
+                  href="https://www.facebook.com/ConfectioneryofCumilla"
+                  target="_blank"
                   title="Facebook"
                   className="flex items-center p-1"
                 >
@@ -38,23 +78,11 @@ const Footer = () => {
                     <path d="M32 16c0-8.839-7.167-16-16-16-8.839 0-16 7.161-16 16 0 7.984 5.849 14.604 13.5 15.803v-11.177h-4.063v-4.625h4.063v-3.527c0-4.009 2.385-6.223 6.041-6.223 1.751 0 3.584 0.312 3.584 0.312v3.937h-2.021c-1.984 0-2.604 1.235-2.604 2.5v3h4.437l-0.713 4.625h-3.724v11.177c7.645-1.199 13.5-7.819 13.5-15.803z"></path>
                   </svg>
                 </a>
+
                 <a
                   rel="noopener noreferrer"
-                  href="#"
-                  title="Twitter"
-                  className="flex items-center p-1"
-                >
-                  <svg
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5 fill-current"
-                  >
-                    <path d="M23.954 4.569a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.691 8.094 4.066 6.13 1.64 3.161a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.061a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.937 4.937 0 004.604 3.417 9.868 9.868 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63a9.936 9.936 0 002.46-2.548l-.047-.02z"></path>
-                  </svg>
-                </a>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
+                  href="https://www.instagram.com/masudiom98/"
+                  target="_blank"
                   title="Instagram"
                   className="flex items-center p-1"
                 >
@@ -70,36 +98,26 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          {/* <ul className="flex flex-wrap items-center space-x-4 sm:space-x-8">
-				<li>
-					<a rel="noopener noreferrer" href="#">Terms of Use</a>
-				</li>
-				<li>
-					<a rel="noopener noreferrer" href="#">Privacy Policy</a>
-				</li>
-			</ul> */}
-          <div className="lg:w-1/4 flex gap-6">
-            <a href="#">
-              {" "}
-              <p className="font-bold">Privacy Policy</p>
-            </a>
-
-            <a href="#">
-              <p className="font-bold">Terms of service</p>
-            </a>
-          </div>
         </div>
-        <div className="mt-3 flex items-center justify-center  py-2 text-secondary-foreground">
-          <p>
-            Copyright © {new Date().getFullYear()} by{" "}
-            <Link
-              to="/"
-              className="underline underline-offset-2 hover:text-primary hover:font-bold"
-              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            >
-              SkillTrack
-            </Link>
-          </p>
+        <div className="lg:flex lg:justify-between pt-5">
+          <div>
+            <p className="font-semibold pb-2">pay with</p>
+            <p className="bg-white inline-block p-3 rounded-lg font-bold text-blue-700">
+              Stripe
+            </p>
+          </div>
+          <div className="pt-5">
+            <p className="font-semibold">
+              Copyright © {new Date().getFullYear()} by{" "}
+              <Link
+                to="/"
+                className="underline underline-offset-2 hover:text-primary hover:font-bold"
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+              >
+                SkillTrack
+              </Link>
+            </p>
+          </div>
         </div>
       </footer>
     </div>
