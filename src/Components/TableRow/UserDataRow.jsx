7569bf65877f7334a7ecfb10611dc11e25bd327e
masud467 <masudpfrnt@gmail.com>
2024-11-29 15:46:37 +0600

    staged changes.

--- src/Components/TableRow/UserDataRow.jsx
+++ src/Components/TableRow/UserDataRow.jsx
@@ -1,114 +1,118 @@
-import PropTypes from 'prop-types'
-import { useState } from 'react';
-import UpdateRoleModal from '../Modal/UpdateRoleModal';
+import PropTypes from "prop-types";
+import { useState } from "react";
+import UpdateRoleModal from "../Modal/UpdateRoleModal";
 
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import toast from 'react-hot-toast';
-import Swal from 'sweetalert2';
-
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import toast from "react-hot-toast";
+import Swal from "sweetalert2";
 
 const UserDataRow = ({ user, refetch }) => {
-   
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen,setIsOpen] =useState(false)
+  const { mutateAsync } = useMutation({
+    mutationFn: async (role) => {
+      const { data } = await axios.patch(
+        `http://localhost:6003/user/update/${user?.email}`,
+        role
+      );
+      return data;
+    },
+    onSuccess: (data) => {
+      refetch();
+      console.log(data);
+      toast.success("user role updated successfully");
+      setIsOpen(false);
+    },
+  });
 
-    const { mutateAsync} = useMutation({
-        mutationFn:async role=>{
-            const {data}=await axios.patch(`http://localhost:6003/user/update/${user?.email}`,role)
-            return data
-        },
-         onSuccess:data=>{
-            refetch()
-            console.log(data)
-            toast.success('user role updated successfully')
-            setIsOpen(false)
-         }
-    })
-
-    // modal handler
-    const modalHandler = async selected=>{
-        console.log('user role change',selected)
-        const userRole={
-            role:selected,
-            status:"verified"
-        }
-
-        try{
-            await mutateAsync(userRole)
-           
-        }catch(err){
-            console.log(err)
-            toast.error(err.message)
-        }
-    }
-    
-    const handleMakeAdminClick = () => {
-      if (user?.role === 'admin') {
-        Swal.fire({
-          icon: 'info',
-          title: 'Already an Admin',
-          text: 'This user is already an admin.',
-        });
-      } else {
-        setIsOpen(true);
-      }
+  // modal handler
+  const modalHandler = async (selected) => {
+    console.log("user role change", selected);
+    const userRole = {
+      role: selected,
+      status: "verified",
     };
-    return (
-        
-             <tr>
-                <td>
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle w-12 h-12">
-                <img src={user?.image} alt="Avatar Tailwind CSS Component" />
-              </div>
+
+    try {
+      await mutateAsync(userRole);
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+  };
+
+  const handleMakeAdminClick = () => {
+    if (user?.role === "admin") {
+      Swal.fire({
+        icon: "info",
+        title: "Already an Admin",
+        text: "This user is already an admin.",
+      });
+    } else {
+      setIsOpen(true);
+    }
+  };
+  return (
+    <tr>
+      <td>
+        <div className="flex items-center gap-3">
+          <div className="avatar">
+            <div className="mask mask-squircle w-12 h-12">
+              <img src={user?.image} alt="Avatar Tailwind CSS Component" />
             </div>
-           
           </div>
-        </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>{user?.email}</p>
+        </div>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>{user?.name}</p>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{user?.email}</p>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>{user?.role}</p>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{user?.name}</p>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{user?.role}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         {user?.status ? (
           <p
             className={`${
-              user.status === 'pending' ? 'text-green-500' : 'text-yellow-500'
+              user.status === "pending" ? "text-green-500" : "text-yellow-500"
             } whitespace-no-wrap`}
           >
             {user.status}
           </p>
         ) : (
-          <p className='text-red-500 whitespace-no-wrap'>Unavailable</p>
+          <p className="text-red-500 whitespace-no-wrap">Unavailable</p>
         )}
       </td>
 
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <button onClick={handleMakeAdminClick} className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight' >
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <button
+          onClick={handleMakeAdminClick}
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+        >
           <span
-            aria-hidden='true'
-            className='absolute inset-0 bg-green-200 opacity-50 rounded-full'
+            aria-hidden="true"
+            className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
           ></span>
-          <span className='relative'>Make Admin</span>
+          <span className="relative">Make Admin</span>
         </button>
         {/* Update User Modal */}
-        <UpdateRoleModal isOpen={isOpen} setIsOpen={setIsOpen} modalHandler={modalHandler} user={user} ></UpdateRoleModal>
+        <UpdateRoleModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          modalHandler={modalHandler}
+          user={user}
+        ></UpdateRoleModal>
       </td>
     </tr>
-       
-    );
+  );
 };
 
 UserDataRow.propTypes = {
-    user: PropTypes.object,
-    refetch: PropTypes.func,
-  }
+  user: PropTypes.object,
+  refetch: PropTypes.func,
+};
 
 export default UserDataRow;
