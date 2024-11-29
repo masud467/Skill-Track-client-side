4a3a64eaffc2b0d501e53599002b04e7538d392a
masud467 <masudpfrnt@gmail.com>
2024-11-29 15:51:52 +0600

    changed the server side url.

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const axiosPublic= axios.create({
-    baseURL:'https://skill-track-for-server.vercel.app'
-})
+export const axiosPublic = axios.create({
+  baseURL: "http://localhost:6003/",
+});
 
 const useAxiosPublic = () => {
-    return axiosPublic
+  return axiosPublic;
 };
 
 export default useAxiosPublic;
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,30 +4,30 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export const axiosSecure = axios.create({
-    baseURL: 'https://skill-track-for-server.vercel.app',
-    withCredentials: true,
-  })
+  baseURL: "http://localhost:6003/",
+  withCredentials: true,
+});
 
 const useAxiosSecure = () => {
-    const { logOut } = useAuth()
-    const navigate = useNavigate()
-    useEffect(() => {
-      axiosSecure.interceptors.response.use(
-        res => {
-          return res
-        },
-        async error => {
-          console.log('status error in the interceptor', error.response)
-          if (error.response.status === 401 || error.response.status === 403) {
-            await logOut()
-            navigate('/login')
-          }
-          return Promise.reject(error)
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      async (error) => {
+        console.log("status error in the interceptor", error.response);
+        if (error.response.status === 401 || error.response.status === 403) {
+          await logOut();
+          navigate("/login");
         }
-      )
-    }, [logOut, navigate])
-  
-    return axiosSecure
+        return Promise.reject(error);
+      }
+    );
+  }, [logOut, navigate]);
+
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
